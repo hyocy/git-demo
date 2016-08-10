fcee488fca40ae44586430e3187fc1e2898678fd
stu13 <stu13@stu13-PC>
2016-08-10 10:46:37 +0900

    test

--- gitTest/src/main/java/com/test/gittest/HomeController.java
+++ gitTest/src/main/java/com/test/gittest/HomeController.java
@@ -32,7 +32,9 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("Hello Git");
+		System.out.println("Test");
+		System.out.println("Test Again");
 		return "home";
 	}
 	
